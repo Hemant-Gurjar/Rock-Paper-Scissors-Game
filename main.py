0ddeaa639aa746cd08e9498a7bf183906c296f1d
Hemant Choudhary <globalgrowth07@gmail.com>
2022-01-12 09:21:43 +0000

    expected final version

--- main.py
+++ main.py
@@ -27,21 +27,25 @@ scissors = '''
 
 #Write your code below this line 👇
 import random
-
+game_picture = [rock, paper, scissors]
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. "))
-computer_choice = random.randint(0, 2)
-
-print(computer_choice)
-if user_choice == 0:
-  print(rock)
-elif user_choice == 1:
-  print(paper)
+if user_choice >= 3 or user_choice < 0:
+  print("YOU TYPED A WRONG NUMBER. YOU LOOSE")
 else:
-  print(scissors)
+  print("You choose")
+  print(game_picture[user_choice])
+ 
+  computer_choice = random.randint(0, 2)
+  print(f"Computer chooses:")
+  print(game_picture[computer_choice])
 
-if computer_choice == 0:
-    print(paper)
-elif computer_choice == 1:
-  print(rock)
-else:
-  print(scissors)
+  if user_choice == 0 and computer_choice == 2:
+    print("YOU WIN")
+  elif computer_choice == 0 and user_choice == 2:
+    print("YOU LOOSE")
+  elif computer_choice > user_choice:
+    print("YOU LOOSE")
+  elif user_choice > computer_choice:
+    print("YOU WIN")
+  elif computer_choice == user_choice:
+    print("IT'S A DRAW")
