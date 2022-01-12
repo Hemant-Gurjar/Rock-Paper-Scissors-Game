600ecd83ec90cdc2cb6c72e3aa220ffd395d06a9
Hemant Choudhary <globalgrowth07@gmail.com>
2022-01-12 08:44:45 +0000

    user can choose and computer will make random choice.

--- main.py
+++ main.py
@@ -26,4 +26,22 @@ scissors = '''
 '''
 
 #Write your code below this line 👇
+import random
 
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. "))
+computer_choice = random.randint(0, 2)
+
+print(computer_choice)
+if user_choice == 0:
+  print(rock)
+elif user_choice == 1:
+  print(paper)
+else:
+  print(scissors)
+
+if computer_choice == 0:
+    print(paper)
+elif computer_choice == 1:
+  print(rock)
+else:
+  print(scissors)
